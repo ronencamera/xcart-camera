22879137f2a7f7dbba397211da385f8bfb5f5a0b
Ronen <ronen@camera51.com>
2016-12-11 18:13:02 +0200

    fix when new product

---
+++ classes/XLite/Module/XC/Camera51/Controller/Admin/Product.php
@@ -0,0 +1,19 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Copyright (c) 2011-present Qualiteam software Ltd. All rights reserved.
+ * See https://www.x-cart.com/license-agreement.html for license details.
+ */
+
+namespace XLite\Module\XC\Camera51\Controller\Admin;
+
+/**
+ * Product controller
+ */
+class Product extends \XLite\Controller\Admin\AAdmin
+{
+    // {{{ Pages
+
+
+}
---
+++ classes/XLite/Module/XC/Camera51/View/Page/Admin/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace XLite\Module\XC\Camera51\View\Page\Admin;
+
+
+/**
+ * @ListChild (list="admin.center", zone="admin")
+ */
+
+class Product extends \XLite\View\AView
+{
+
+
+    /**
+     * Return widget default template
+     *
+     * @return string
+     */
+    protected function getDefaultTemplate()
+    {
+
+
+        return 'modules/XC/Camera51/option/product.twig';
+    }
+
+    public function getJSFiles()
+    {
+        $list = parent::getJSFiles();
+
+        $list[] = array(
+            'file'      => 'modules/XC/Camera51/option/component.js',
+            'no_minify' => true
+        );
+        return $list;
+    }
+
+
+    public static function getAllowedTargets()
+    {
+        return array_merge(parent::getAllowedTargets(), array('product'));
+    }
+
+}
